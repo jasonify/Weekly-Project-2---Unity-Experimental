afe7252e5cf5689ae772c83c781ceab4f3fbde0b
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-28 19:54:54 -0800

    Pill Script for basic rotation

---
+++ WeeklyProject2/Assets/PillScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillScript : MonoBehaviour {
+    public Vector3 spinSpeed = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(0, 5, 0);
+        this.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+
+        this.spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(spinSpeed);
+	}
+}

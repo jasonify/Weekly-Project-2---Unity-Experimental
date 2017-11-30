62fcb0b67aff4f94cd69482ffc7d3099c15756f0
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-29 18:39:04 -0800

    Rotate thing

--- WeeklyProject2/Assets/PillScript.cs
+++ WeeklyProject2/Assets/PillScript.cs
@@ -6,7 +6,7 @@ public class PillScript : MonoBehaviour {
     public Vector3 spinSpeed = Vector3.zero;
 	// Use this for initialization
 	void Start () {
-        this.transform.position = new Vector3(0, 5, 0);
+        this.transform.position = new Vector3(0, 5, 3);
         this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
         this.spinSpeed = new Vector3(Random.value, Random.value, Random.value);
@@ -25,6 +25,7 @@ public class PillScript : MonoBehaviour {
         this.spinSpeed.y *= 2;
         this.spinSpeed.z *= 2;
     }
+
     private void OnMouseEnter()
     {
         Debug.Log("Mouse over", gameObject);

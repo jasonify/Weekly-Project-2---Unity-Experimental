2c3dd3870748727d4fc069ac1e8656ae632cef60
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-29 19:02:59 -0800

    Rotate opposite direction

--- WeeklyProject2/Assets/PillScript.cs
+++ WeeklyProject2/Assets/PillScript.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PillScript : MonoBehaviour {
+    public Vector3 rotateAround = new Vector3(0, 1, 0);
+    public float rotateSpeed = 1.0f;
     public Vector3 spinSpeed = Vector3.zero;
 	// Use this for initialization
 	void Start () {
-        this.transform.position = new Vector3(0, 5, 3);
+        // this.transform.position = new Vector3(0, 5, 3);
         this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
         this.spinSpeed = new Vector3(Random.value, Random.value, Random.value);
@@ -15,8 +17,13 @@ public class PillScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.transform.Rotate(spinSpeed);
-        this.transform.RotateAround(Vector3.zero, Vector3.up, 1.0f);
+        this.transform.RotateAround(Vector3.zero, rotateAround, 1.0f);
 	}
+
+    public void setScale(float scale)
+    {
+        this.transform.localScale = new Vector3(scale, scale, scale);
+    }
 
     private void OnMouseDown()
     {
@@ -24,6 +31,7 @@ public class PillScript : MonoBehaviour {
         this.spinSpeed.x *= 2;
         this.spinSpeed.y *= 2;
         this.spinSpeed.z *= 2;
+        rotateAround.y = -1 * rotateAround.y;
     }
 
     private void OnMouseEnter()

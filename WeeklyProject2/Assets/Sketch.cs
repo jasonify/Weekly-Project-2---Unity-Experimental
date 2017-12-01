cf9b93d81f5ba700de09f8b90a94f88b197ee81d
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-30 19:53:40 -0800

    Make spinecube

--- WeeklyProject2/Assets/Sketch.cs
+++ WeeklyProject2/Assets/Sketch.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Sketch : MonoBehaviour {
-
+    public GameObject myPrefab;
 	// Use this for initialization
 	void Start () {
-		
+        var newPill = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
 	}
 	
 	// Update is called once per frame

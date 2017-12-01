b479848859fd6af45be3d9434f6f2fc5861cb659
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-30 20:11:06 -0800

    Rotate multiple

--- WeeklyProject2/Assets/Sketch.cs
+++ WeeklyProject2/Assets/Sketch.cs
@@ -6,7 +6,15 @@ public class Sketch : MonoBehaviour {
     public GameObject myPrefab;
 	// Use this for initialization
 	void Start () {
-        var newPill = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        int totalCubes = 6;
+        for (int i = 0; i < totalCubes; i++)
+        {
+            float x = 3.0f;
+            float y = 5.0f;
+            float z = 0.0f;
+            var newPill = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newPill.GetComponent<PillScript>().setScale(1.2f);
+        }
 	}
 	
 	// Update is called once per frame

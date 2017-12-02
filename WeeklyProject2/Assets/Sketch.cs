b0bd239b95f8e478bef951de794212ace7cf61ea
Jason Bautista <jasoncb@cs.stanford.edu>
2017-12-01 19:37:27 -0800

    Code cleanup

--- WeeklyProject2/Assets/Sketch.cs
+++ WeeklyProject2/Assets/Sketch.cs
@@ -10,15 +10,13 @@ public class Sketch : MonoBehaviour {
         for (int i = 0; i < totalCubes; i++)
         {
             float perc = i / (float) totalCubes;
-            Debug.Log("---one");
-            Debug.Log(perc.ToString());
             float x = perc * 3.0f;
             float y = 5.0f;
             float z = 0.0f;
 
             var newPill = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
             newPill.GetComponent<PillScript>().SetScale(perc);
-            newPill.GetComponent<PillScript>().rotateSpeed = 0.0f; // Random.value; 
+            newPill.GetComponent<PillScript>().rotateSpeed = Random.value + 1.0f; 
         }
 	}
 	

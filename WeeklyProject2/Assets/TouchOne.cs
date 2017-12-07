82f1444837bbda9a6685a813b48b20ea736d2744
Jason Bautista <jasoncb@cs.stanford.edu>
2017-12-06 23:58:42 -0800

    TouchOne redone

--- WeeklyProject2/Assets/TouchOne.cs
+++ WeeklyProject2/Assets/TouchOne.cs
@@ -11,14 +11,29 @@ public class TouchOne : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        float scale = 0.05f;
         if(OVRInput.GetDown(OVRInput.Button.One))
         {
             Debug.Log("input A");
+
+            GameObject s;
+            s = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            // s.transform.position = new Vector3((Random.value * 461) + 10, (Random.value * 300) + 10, 0F);
+            s.transform.position = this.transform.position;
+
+            s.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
         }
 
         if (OVRInput.GetDown(OVRInput.Touch.Two))
         {
+            GameObject s;
+
             Debug.Log("input B");
+            s = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            // s.transform.position = new Vector3((Random.value * 461) + 10, (Random.value * 300) + 10, 0F);
+            s.transform.position = this.transform.position;
+            s.transform.localScale = new Vector3(scale, scale, scale);
+
         }
 
     }

6dc84eb39e2f8156ceeab70b3584f600540eabfa
Jason Bautista <jasoncb@cs.stanford.edu>
2017-12-10 16:01:46 -0800

    Weapon hit

---
+++ WeeklyProject2/Assets/WeaponHit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (OVRInput.GetUp(OVRInput.RawButton.X) )
+        {
+            Debug.Log("HELLO");
+        }       
+	}
+}

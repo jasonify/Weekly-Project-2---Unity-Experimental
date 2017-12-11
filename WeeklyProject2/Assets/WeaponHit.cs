ca173cf26116acaaef0ba358559b8ae48e45ffb9
Jason Bautista <jasoncb@cs.stanford.edu>
2017-12-10 16:49:21 -0800

    Raycasthit

--- WeeklyProject2/Assets/WeaponHit.cs
+++ WeeklyProject2/Assets/WeaponHit.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (LineRenderer))]
+
 public class WeaponHit : MonoBehaviour {
+
+    public LineRenderer lr;
 
 	// Use this for initialization
 	void Start () {
@@ -12,10 +16,20 @@ public class WeaponHit : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider)
+            {
+                lr.SetPosition(1, new Vector3(0,0, hit.distance));
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             Debug.Log("HELLO");
+
         }
     }
 		
